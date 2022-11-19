03bbd2e1edac073769473cb79d4302d37757f085
Park Sang-Yeop <112646715+2201204@users.noreply.github.com>
2022-11-19 13:29:59 +0900

    Update turtle.py

--- turtle.py
+++ turtle.py
@@ -1,9 +1,9 @@
 4.
-import turtle
-import random
-t = turtle.Turtle()
+import turtle                 # 터틀 라이브러리 불러옴
+import random                 # 랜덤 라이브러리 불러옴
+t = turtle.Turtle() 
 t.shape("turtle")
-def draw_square(x, y, c):
+def draw_square(x, y, c):     # 사각형 그리기
     t.up()
     t.goto(x, y)
     t.down()
@@ -25,15 +25,15 @@ for c in ["yellow", "red", "purple", "blue"]:
    y = random.randint(-100, 100)
    draw_square(x, y, c)
 5.
-import turtle
-import random
-t = turtle.Turtle()
+import turtle                  # 터틀 라이브러리 불러옴
+import random                  # 랜덤 라이브러리 불러옴
+t = turtle.Turtle()            
 s = turtle.Screen()
 def draw_shape(t, c, length, sides, x, y):
     t.up()
-    t.goto(x, y)
+    t.goto(x, y)               # 좌표이동
     t.down()
-    t.fillcolor(c)
+    t.fillcolor(c)             # 색선정
     angle = 360.0 / sides
     t.begin_fill()
     for dist in range(sides):
@@ -43,7 +43,7 @@ def draw_shape(t, c, length, sides, x, y):
 
     
 for i in range(10):
-    color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])
+    color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])  # 색 리스트
     side_length = random.randint(10, 100)
     sides = random.randint(3, 10)
     x = random.randint(-200, 200)
@@ -62,14 +62,14 @@ def draw_star(aturtle, colour,length, x, y):
     t.goto(x, y)
     t.pendown()
     t.begin_fill()
-    for i in range(5):
+    for i in range(5):                   #  5번 반복 도형그리기
         t.forward(length)
         t.right(144)
         t.forward(length)
     t.end_fill()
 
     
-for i in range(20):
+for i in range(20):                        # 별20개 그리기
     color = random.choice([ 'white', 'yellow', 'blue', 'skyblue', 'orange', 'green' ])
     side_length = random.randint(10, 100)
     x = random.randint(-200, 200)
